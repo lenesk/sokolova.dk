eb3b65cad5a7c86b218b5f3675c043162b9f8292
Elena Sokolova <elena@sokolova.dk>
2012-08-05 13:05:46 +0200

    files ready for navigation

--- generate.py
+++ generate.py
@@ -3,8 +3,11 @@ import sys
 from markdown import markdown
 
 
+template = open('TEMPLATE.html').read()
+
 for file in sys.argv[1:]:
     new_file = file.rstrip('.md') + '.html'
     print '%s -> %s' % (file, new_file)
     html = markdown(open(file).read())
+    html = template.replace('{{body}}', html)
     open(new_file, 'w').write(html.encode('utf8'))
